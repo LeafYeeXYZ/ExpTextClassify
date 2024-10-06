8a6a03eeeda088d675f0721732e810680dd8ae60
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-10-06 16:08:49 +0800

    init

--- src/trails/ending.ts
+++ src/trails/ending.ts
@@ -3,7 +3,7 @@ import htmlButtonResponse from '@jspsych/plugin-html-button-response'
 export const endingTrails: any[] = [
   {
     type: htmlButtonResponse,
-    stimulus: '这里将让被试选择自己是否认真实验',
+    stimulus: '这里将让被试选择自己是否认真实验', // 委婉一点, 用一些降低威胁性的手段
     choices: ['是', '否'],
   },
   {
@@ -13,7 +13,7 @@ export const endingTrails: any[] = [
   },
   {
     type: htmlButtonResponse,
-    stimulus: '这里将让被试选择是否愿意上传实验数据',
+    stimulus: '这里将让被试选择是否愿意上传实验数据', // 包括委婉地告诉猜到了实验目的的被试不要上传数据
     choices: ['是', '否'],
   },
 ]
